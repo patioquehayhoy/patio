f3ff9422eeaeb32f05f6cc806d1ac897e793abc4
Parco <parco@Parcos-MacBook-Pro.local>
2026-03-22 19:09:27 -0600

    v1.9 - login-callback getSessionFromUrl

--- app/login-callback.tsx
+++ app/login-callback.tsx
@@ -1,16 +1,25 @@
 import { useEffect } from 'react'
-import { router } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { supabase } from '@/lib/supabase'
+import { Linking } from 'react-native'
 
 export default function LoginCallback() {
+  const router = useRouter()
+
   useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      if (data.session?.user?.email) {
-        router.replace('/menu')
+    Linking.getInitialURL().then(async (url) => {
+      if (url) {
+        const { data, error } = await supabase.auth.getSessionFromUrl({ url } as any)
+        if (data?.session?.user?.email) {
+          router.replace('/menu')
+        } else {
+          router.replace('/')
+        }
       } else {
         router.replace('/')
       }
     })
   }, [])
+
   return null
 }
